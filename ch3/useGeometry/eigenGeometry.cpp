fb9e6bd61cf807f3d49437a5746a2854080bb3d0
MrCocoaCat <liyubo_liyubo@163.com>
2017-12-17 13:03:47 +0800

    代码规范修改建议
    cout 调用成员函数，加空格不易理解

--- ch3/useGeometry/eigenGeometry.cpp
+++ ch3/useGeometry/eigenGeometry.cpp
@@ -17,7 +17,7 @@ int main ( int argc, char** argv )
     Eigen::Matrix3d rotation_matrix = Eigen::Matrix3d::Identity();
     // 旋转向量使用 AngleAxis, 它底层不直接是Matrix，但运算可以当作矩阵（因为重载了运算符）
     Eigen::AngleAxisd rotation_vector ( M_PI/4, Eigen::Vector3d ( 0,0,1 ) );     //沿 Z 轴旋转 45 度
-    cout .precision(3);
+    cout.precision(3);
     cout<<"rotation matrix =\n"<<rotation_vector.matrix() <<endl;                //用matrix()转换成矩阵
     // 也可以直接赋值
     rotation_matrix = rotation_vector.toRotationMatrix();

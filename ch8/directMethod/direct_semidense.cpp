06b1c5a981b51eae933b32aca37b669156e5847b
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-03-21 17:14:00 +0800

    fix Usage message issue

--- ch8/directMethod/direct_semidense.cpp
+++ ch8/directMethod/direct_semidense.cpp
@@ -158,7 +158,7 @@ int main ( int argc, char** argv )
 {
     if ( argc != 2 )
     {
-        cout<<"usage: useLK path_to_dataset"<<endl;
+        cout<<"usage: direct_semidense path_to_dataset"<<endl;
         return 1;
     }
     srand ( ( unsigned int ) time ( 0 ) );

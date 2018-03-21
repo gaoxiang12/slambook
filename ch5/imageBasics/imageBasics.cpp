4f01704e91b5bebf6799b53d3a268a144019668d
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-03-21 16:46:55 +0800

    should print Usage Message while launch it without input image

--- ch5/imageBasics/imageBasics.cpp
+++ ch5/imageBasics/imageBasics.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 int main ( int argc, char** argv )
 {
+    if(argc < 2)
+    {
+        cout << "Usage: ./imageBasics image_file_name" << endl;
+        return -1;
+    }
     // 读取argv[1]指定的图像
     cv::Mat image;
     image = cv::imread ( argv[1] ); //cv::imread函数读取指定路径下的图像
